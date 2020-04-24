9945f4d84d45b6efaf65e62931c120d50a533a5d
Knut Ivar Alvestad <knut.alvestad@evry.com>
2020-04-24 12:49:36 +0200

    fix init mod

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -10,7 +10,7 @@ import (
 	pb "github.com/knutia/shippy/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-)cd
+)
 
 const (
 	port = ":50051"
